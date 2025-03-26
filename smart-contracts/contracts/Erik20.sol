b251b2ee6cb88d41c88032397e271aca8c3589c0
vctrubio <vctrubio@gmail.com>
2025-03-26 01:09:16 +0100

    erik20 init

--- smart-contracts/contracts/Erik20.sol
+++ smart-contracts/contracts/Erik20.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
-contract Erik20 {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Erik20 is ERC20 {
     uint256 public myVariable;
 
-    constructor() {
+    constructor() ERC20("ErikToken", "ERIK") {
         myVariable = 5;
+        _mint(msg.sender, 1000 * 10 ** decimals()); // Mint initial supply to the deployer
     }
- 
 }
