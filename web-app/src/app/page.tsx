6c8b0e20d025efc952ce76f140faae67f82d89e3
vctrubio <vctrubio@gmail.com>
2025-03-28 23:59:26 +0100

    Home titor

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -1,7 +1,32 @@
+function LogoChild() {
+  return (
+    <div className="logo-child">
+      <div>a</div>
+      <div>a</div>
+      <div>a</div>
+      <div>a</div>
+    </div>
+  );
+}
 export default function Home() {
   return (
-    <div className="p-4 m-4 border text-center rounded-lg">
-      my app
+    <div className="full-container">
+      <div className="titor">
+        <div className="avatar">
+          <div className="icon-a"></div>
+          <div className="icon-b"></div>
+        </div>
+        <div className="logo">
+          <LogoChild />
+          <LogoChild />
+          <LogoChild />
+          <LogoChild />
+        </div>
+        <div>quotes</div>
+      </div>
+      <div className="rito">
+        b-page
+      </div>
     </div>
   );
 }
