d83f928cd7afed67f07ec30960aed2c6ba00baa4
vctrubio <vctrubio@gmail.com>
2025-04-01 17:32:37 +0200

    refactoral.contracts.v1

--- web-app/src/components/w3/ContractContent.tsx
+++ web-app/src/components/w3/ContractContent.tsx
@@ -1,15 +1,17 @@
 import { useUser } from '@/lib/UserContext';
 import ContractCreate from './ContractCreate';
+
+
 export const ContractContent = () => {
     const { contract, setContract } = useUser();
 
+    function handleCreateContract(newContract: any) {
+        console.log("Contract created from here:", newContract);
+    }
+
     return (
         <ContractCreate
-            handleCreateContract={(newContract) => {
-                // setContract(newContract);
-                console.log("Contract created:", newContract);
-            }
-            }
+            handleCreateContract={handleCreateContract}
         />
     )
 }
