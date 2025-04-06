b631473564dd4b7b7754e22fc82f66dc0b792f18
vctrubio <vctrubio@gmail.com>
2025-04-06 23:22:35 +0200

    erikv2

--- smart-contracts/contracts/Erik116.sol
+++ smart-contracts/contracts/Erik116.sol
@@ -3,66 +3,124 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-contract Erik is ERC1155 {
+interface IErik {
+    function mint(address to, uint256 id, uint256 amount) external;
+    function burn(address from, uint256 id, uint256 amount) external;
+    function balanceOf(
+        address account,
+        uint256 id
+    ) external view returns (uint256);
+}
+
+contract Erik is ERC1155, IErik {
     uint256 public constant SEED = 0;
     uint256 public constant WATER = 1;
     uint256 public constant SOIL = 2;
 
-    // Token FORGE
-    uint256 public constant PLANT = 3; // Forged from SEED + WATER
-    uint256 public constant FRUIT = 4; // Forged from WATER + SOIL
-    uint256 public constant FLOWER = 5; // Forged from SEED + SOIL
-    uint256 public constant BASKET = 6; // Forged from SEED + WATER + SOIL
+    uint256 public constant PLANT = 3;
+    uint256 public constant FRUIT = 4;
+    uint256 public constant FLOWER = 5;
+    uint256 public constant BASKET = 6;
 
+    //todo: create assets
     constructor() ERC1155("https://myapi.com/metadata/{id}.json") {}
 
-    function mint(address to, uint256 id) external {
-        if (id == SEED || id == WATER || id == SOIL) {
-            _mint(to, id, 1, "");
+    function mint(address to, uint256 id, uint256 amount) external override {
+        require(id >= 0 && id <= 6, "Invalid token ID");
+        _mint(to, id, amount, "");
+    }
+
+    function burn(address from, uint256 id, uint256 amount) external override {
+        require(id >= 0 && id <= 6, "Invalid token ID");
+        _burn(from, id, amount);
+    }
+}
+
+contract ErikForge {
+    IErik public token;
+    uint256 public constant COOLDOWN = 5 seconds;
+    mapping(address => uint256) public lastMintTime;
+
+    constructor(IErik _token) {
+        token = _token;
+    }
+
+    function mint(uint256 tokenId) public {
+        if (tokenId >= 0 && tokenId <= 2) {
+            require(
+                block.timestamp >= lastMintTime[msg.sender] + COOLDOWN,
+                "Cooldown not elapsed"
+            );
+            token.mint(msg.sender, tokenId, 1);
+            lastMintTime[msg.sender] = block.timestamp;
+        } else if (tokenId == 3) {
+            require(
+                token.balanceOf(msg.sender, 0) > 0 &&
+                    token.balanceOf(msg.sender, 1) > 0,
+                "Need SEED and WATER to forge PLANT"
+            );
+            token.burn(msg.sender, 0, 1);
+            token.burn(msg.sender, 1, 1);
+            token.mint(msg.sender, tokenId, 1);
+        } else if (tokenId == 4) {
+            require(
+                token.balanceOf(msg.sender, 1) > 0 &&
+                    token.balanceOf(msg.sender, 2) > 0,
+                "Need WATER and SOIL to forge FRUIT"
+            );
+            token.burn(msg.sender, 1, 1);
+            token.burn(msg.sender, 2, 1);
+            token.mint(msg.sender, tokenId, 1);
+        } else if (tokenId == 5) {
+            require(
+                token.balanceOf(msg.sender, 0) > 0 &&
+                    token.balanceOf(msg.sender, 2) > 0,
+                "Need SEED and SOIL to forge FLOWER"
+            );
+            token.burn(msg.sender, 0, 1);
+            token.burn(msg.sender, 2, 1);
+            token.mint(msg.sender, tokenId, 1);
+        } else if (tokenId == 6) {
+            require(
+                token.balanceOf(msg.sender, 0) > 0 &&
+                    token.balanceOf(msg.sender, 1) > 0 &&
+                    token.balanceOf(msg.sender, 2) > 0,
+                "Need SEED, WATER and SOIL to forge BASKET"
+            );
+            token.burn(msg.sender, 0, 1);
+            token.burn(msg.sender, 1, 1);
+            token.burn(msg.sender, 2, 1);
+            token.mint(msg.sender, tokenId, 1);
         } else {
             revert("Invalid token ID");
         }
     }
 
-    function burn(address to, uint256 id) external {
-        if (id == PLANT || id == FRUIT || id == FLOWER || id == BASKET) {
-            _burn(to, id, 1);
-        } else {
-            revert("Invalid token ID"); // Fixed typo "Invalid to ken ID"
-        }
-    }
-}
-
-contract ErikForge {
-    Erik public token;
-    uint256 public constant COOLDOWN = 5 seconds;
-    mapping(address => uint256) public lastMintTime; // Track last mint time per user
-
-    constructor(Erik _token) {
-        token = _token;
-    }
-
-    // Added mint function to call token.mint with cooldown
-    function mint(uint256 id) public {
-        require(
-            block.timestamp >= lastMintTime[msg.sender] + COOLDOWN,
-            "Cooldown not elapsed"
-        );
-        token.mint(msg.sender, id); // Delegate to token
-        lastMintTime[msg.sender] = block.timestamp;
-    }
-
     function burn(uint256 id) public {
-        require(token.balanceOf(msg.sender, id) > 0, "you dont own this token");
-        token.burn(msg.sender, id);
+        require(id >= 3 && id <= 6, "Invalid token ID");
+        require(
+            token.balanceOf(msg.sender, id) > 0,
+            "You don't own this token"
+        );
+        token.burn(msg.sender, id, 1);
     }
 
     function trade(uint256 tokenIn, uint256 tokenOut) public {
         require(
-            token.balanceOf(msg.sender, tokenIn) > 0,
-            "You don't have this token"
+            tokenOut >= 0 && tokenOut <= 2,
+            "Can only trade for basic tokens (0-2)"
         );
-        token.burn(msg.sender, tokenIn);
-        token.mint(msg.sender, tokenOut);
+        require(
+            token.balanceOf(msg.sender, tokenIn) > 0,
+            "You don't own this token"
+        );
+        require(
+            block.timestamp >= lastMintTime[msg.sender] + COOLDOWN,
+            "Cooldown not elapsed"
+        );
+
+        token.burn(msg.sender, tokenIn, 1);
+        token.mint(msg.sender, tokenOut, 1);
+        lastMintTime[msg.sender] = block.timestamp;
     }
 }
